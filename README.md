6cc9cb8b7a09b3ee263c3b7463038c5b0764117d
macmacal <mac.aleksandrowicz@gmail.com>
2024-07-16 17:17:24 +0200

    working frame_calibration script

--- README.md
+++ README.md
@@ -8,3 +8,11 @@ Original scripts by Paweł Kolendo (@NieTrawisz) dedicated for **recordings** in
 ## Image calibration
 - `./image_calibration`
 Scripts for pre-processing caputered frames
+
+### Calculate camera calibration
+```bash
+./image_calibration/frame_calibration.py -i "/path/to/calibration/images/folder" --image-format "png" -t 0.2 --grid 9 6
+```
+Type `-h` for help.
+
+### Apply calibration to images
--- image_calibration/frame_calibration.py
+++ image_calibration/frame_calibration.py
@@ -1,13 +1,9 @@
+#!/bin/python3
+
 import argparse
 import cv2
 import numpy as np
 from pathlib import Path
-
-# recordings=["short_test2.avi"]
-recordings = ["nagranie.dav", "nagranie2.dav"]  # -> args.input_folder
-checkerborad_grid = (10, 7)  # -> args.checkerboard_grid
-show_calibration_video = False  # -> args.verbose
-calibration_point_threshold = 0.2  # -> args.threshold
 
 
 def main():
@@ -18,13 +14,16 @@ def main():
     checkerboard_grid = args.checkerboard_grid
 
     # prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
-    objp = np.zeros((checkerborad_grid[0] * checkerborad_grid[1], 3), np.float32)
-    objp[:, :2] = np.mgrid[0 : checkerborad_grid[0], 0 : checkerborad_grid[1]].T.reshape(-1, 2)
+    objp = np.zeros((checkerboard_grid[0] * checkerboard_grid[1], 3), np.float32)
+    objp[:, :2] = np.mgrid[
+        0 : checkerboard_grid[0], 0 : checkerboard_grid[1]
+    ].T.reshape(-1, 2)
 
     # Arrays to store object points and image points from all the images.
     world_points = []  # 3d point in real world space
     image_points = []  # 2d points in image plane
 
+    used_imgs_cnt = 0
     for cnt, path in enumerate(imgs_paths):
         print(f"> Processing image {cnt}/{len(imgs_paths)}: {path}")
         img = cv2.imread(str(path))
@@ -33,34 +32,32 @@ def main():
             raise ValueError(f"Image '{path}' is None")
 
         img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-        checkboard_found, raw_corners = cv2.findChessboardCorners(img_gray, checkerboard_grid, None)
+        checkboard_found, raw_corners = cv2.findChessboardCorners(
+            img_gray, checkerboard_grid, None
+        )
 
         if not checkboard_found:
-            msg = "Not all of the checkerboard points are detected"
+            msg = "ERR: Missing points"
             print(f"> {msg}")
             cv2_put_text(img, msg)
 
         else:
+            used_imgs_cnt += 1
+            corners = cv2.cornerSubPix(
+                img_gray, raw_corners, (11, 11), (-1, -1), criteria
+            )
 
-            corners = cv2.cornerSubPix(img_gray, raw_corners, (11, 11), (-1, -1), criteria)
-
-            if (
-                is_in_threshold_range(corners[0, 0, :].reshape(2), img_gray.shape)
-                or is_in_threshold_range(
-                    corners[checkerboard_grid[0] - 1, 0, :].reshape(2),
-                    img_gray.shape,
-                )
-                or is_in_threshold_range(
-                    corners[-checkerboard_grid[0], 0, :].reshape(2), img_gray.shape
-                )
-                or is_in_threshold_range(corners[-1, 0, :].reshape(2), img_gray.shape)
+            if is_any_checkboard_corner_near_corner(
+                checkerboard_grid, img_gray, corners, args.threshold
             ):
                 world_points.append(objp)
                 image_points.append(corners)
-                cv2.drawChessboardCorners(img, checkerboard_grid, corners, checkboard_found)
+                cv2.drawChessboardCorners(
+                    img, checkerboard_grid, corners, checkboard_found
+                )
 
             else:
-                msg = "Checkerboard is too far from corners"
+                msg = "ERR: Checkerboard is too far from corners"
                 print(f"> {msg}")
                 cv2_put_text(img, msg)
 
@@ -68,12 +65,16 @@ def main():
             cv2.imshow("Preview", img)
             cv2.waitKey(1000)
 
-    print("> Finished image analysis.")
+    print(
+        f"> Finished image analysis. Measurements are taken from {used_imgs_cnt} out of {len(imgs_paths)} images."
+    )
 
     _, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(
         world_points, image_points, img_gray.shape[::-1], None, None
     )
-    print(f"> Calibration mtx={mtx} dist={dist} rvecs={rvecs} tvecs={tvecs}")
+    print(f"> Calculated camera parameters")
+    if args.verbose:
+        print(f"> Calibration params: \n  mtx={mtx}\n  dist={dist}\n  rvecs={rvecs}\n  tvecs={tvecs}")
 
     np.savez(
         args.output_file,
@@ -95,13 +96,15 @@ def parse_args():
         type=Path,
         required=True,
     )
-    parser.add_argument("--image-format", help="Image format extension", default="jpg", type=str)
+    parser.add_argument(
+        "--image-format", help="Image format extension", default="jpg", type=str
+    )
     parser.add_argument(
         "-o",
         "--output-file",
         help="Output directory for the camera calibration",
         type=Path,
-        default="./calibration.yaml",
+        default=None,
     )
     parser.add_argument(
         "-t",
@@ -111,7 +114,9 @@ def parse_args():
         required=True,
         default=0.2,
     )
-    parser.add_argument("-v", "--verbose", help="Show calibration video", action="store_true")
+    parser.add_argument(
+        "-v", "--verbose", help="Show calibration video", action="store_true"
+    )
 
     parser.add_argument(
         "--grid",
@@ -123,7 +128,13 @@ def parse_args():
     )
 
     args = parser.parse_args()
-    args.checkerboard_grid = tuple(args.checkerboard_grid)
+    args.checkerboard_grid = tuple(args.grid)
+    args.img_format = args.image_format
+    
+    if args.output_file is None:
+        args.output_file = args.input_folder.with_name("calibration.npz")
+    else:
+        args.output_file = args.output_file.with_suffix(".npz")
 
     return args
 
@@ -138,7 +149,7 @@ def cv2_put_text(img: cv2.typing.MatLike, text: str) -> None:
     cv2.putText(
         img,
         text,
-        (100, 100),
+        (32, 84),
         cv2.FONT_HERSHEY_SIMPLEX,
         3,
         (0, 0, 255),
@@ -147,13 +158,38 @@ def cv2_put_text(img: cv2.typing.MatLike, text: str) -> None:
     )
 
 
-def is_in_threshold_range(point: np.array, shape: np.array) -> bool:
+def is_any_checkboard_corner_near_corner(
+    checkerboard_grid: tuple[int, int],
+    img_gray: cv2.typing.MatLike,
+    corners: cv2.typing.MatLike,
+    calib_point_threshold: float,
+) -> bool:
+    threshold = calib_point_threshold
+    return (
+        is_in_threshold_range(corners[0, 0, :].reshape(2), img_gray.shape, threshold)
+        or is_in_threshold_range(
+            corners[checkerboard_grid[0] - 1, 0, :].reshape(2),
+            img_gray.shape,
+            threshold,
+        )
+        or is_in_threshold_range(
+            corners[-checkerboard_grid[0], 0, :].reshape(2), img_gray.shape, threshold
+        )
+        or is_in_threshold_range(
+            corners[-1, 0, :].reshape(2), img_gray.shape, threshold
+        )
+    )
+
+
+def is_in_threshold_range(
+    point: np.array, shape: np.array, calib_point_threshold: float
+) -> bool:
     dist = []
     corners = np.array([[0, 0], [0, shape[0]], [shape[1], 0], [shape[1], shape[0]]])
     for corner in corners:
         dist.append(np.sqrt((point[0] - corner[0]) ** 2 + (point[1] - corner[1]) ** 2))
 
-    threshold = np.sqrt(shape[0] ** 2 + shape[1] ** 2) * calibration_point_threshold
+    threshold = np.sqrt(shape[0] ** 2 + shape[1] ** 2) * calib_point_threshold
     return any(d < threshold for d in dist)
 
 
